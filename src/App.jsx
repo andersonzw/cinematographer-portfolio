c5187b98375d66085d05cb97fbe12ce76fd53108
Anderson <84068667+andersonzw@users.noreply.github.com>
2024-05-20 17:06:49 +1000

    project vite init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./routes/Hero";
+
+function App() {
+  return (
+    <div className="bg-gray-900">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='min-h-screen'>s</section>
+  )
+}
+
+export default Hero
