20eaded1abe025b074acf27d5c6a02848e69e8bd
Anderson <84068667+andersonzw@users.noreply.github.com>
2024-05-21 14:58:34 +1000

    added react count-up

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,4 +1,12 @@
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+
 const About = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true, // Ensures the count up happens once
+    threshold: 0.5, // Triggers when 50% of the element is in view
+  });
+
   return (
     <section className="bg-black ">
       <div className="bg-white min-h-[30vh] text-black text-center text-2xl flex p-8">
@@ -16,7 +24,10 @@ const About = () => {
             alt="camera "
             className=" w-full h-full object-cover "
           />
-          <a href="/about" className="bottom-0 left-0 w-full h-full absolute bg-[rgba(0,0,0,0.76)]  backdrop-blur-md opacity-0 hover:opacity-100 px-8 py-12 flex flex-col items-center justify-center">
+          <a
+            href="/about"
+            className="bottom-0 left-0 w-full h-full absolute bg-[rgba(0,0,0,0.76)]  backdrop-blur-md opacity-0 hover:opacity-100 tiles-hover-transition  px-8 py-12 flex flex-col items-center justify-center"
+          >
             <h1 className="text-4xl font-bold uppercase ">About</h1>
             <p>Samuel Liu</p>
           </a>
@@ -29,7 +40,7 @@ const About = () => {
               alt="camera "
               className=" w-full h-full object-cover"
             />
-            <div className="bottom-0 left-0 w-full h-full absolute bg-[rgba(0,0,0,0.76)]  backdrop-blur-md opacity-0 hover:opacity-100 px-8 py-12 flex flex-col items-center justify-center">
+            <div className="bottom-0 left-0 w-full h-full absolute bg-[rgba(0,0,0,0.76)]  backdrop-blur-md opacity-0 hover:opacity-100 tiles-hover-transition px-8 py-12 flex flex-col items-center justify-center">
               <h1 className="text-4xl font-bold uppercase ">Film Services</h1>
               <p>
                 Events &#8226; Weddings &#8226; Interviews &#8226;
@@ -43,7 +54,7 @@ const About = () => {
               alt="camera "
               className=" w-full h-full object-cover"
             />
-            <div className="bottom-0 left-0 w-full h-full absolute bg-[rgba(0,0,0,0.76)]  backdrop-blur-md opacity-0 hover:opacity-100  px-8 py-12 flex flex-col items-center justify-center">
+            <div className="bottom-0 left-0 w-full h-full absolute bg-[rgba(0,0,0,0.76)]  backdrop-blur-md opacity-0 hover:opacity-100 tiles-hover-transition px-8 py-12 flex flex-col items-center justify-center">
               <h1 className="text-4xl font-bold uppercase ">video editing</h1>
               <p>Drone Reels &#8226; Montages &#8226; Compilations </p>
             </div>
@@ -51,8 +62,17 @@ const About = () => {
         </div>
       </div>
       <div className="bg-white min-h-[30vh] text-black text-center text-2xl flex p-8">
-        <h1 className="max-w-screen-xl mx-auto my-auto">
-          60+ videos produced and edited, 5 years of experience.
+        <h1 ref={ref} className="max-w-screen-xl mx-auto my-auto">
+          {inView && (
+            <span>
+              <CountUp start={1} end={99} duration={3.5} /> +
+            </span>
+          )}
+          {""} videos produced and edited,{" "}
+          <span>
+            <CountUp start={1} end={5} duration={3.5} />
+          </span>{" "}
+          years of experience.
         </h1>
       </div>
     </section>
