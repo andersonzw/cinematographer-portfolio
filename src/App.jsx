59615b89ce9bca229e51244eb4d5582c371d909c
Anderson <84068667+andersonzw@users.noreply.github.com>
2024-05-27 16:12:04 +1000

    added mobile response to tiles

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   
   return (
     <div className="relative w-full">
-
+      
       <Routes>
         <Route path="/" element={<Header />}>
           <Route
--- src/routes/Reel.jsx
+++ src/routes/Reel.jsx
@@ -1,21 +1,43 @@
 import { useContext } from "react";
 import { ScrollContext } from "../util/ScrollContext";
+import CountUp from "react-countup";
+import {  useInView } from "framer-motion";
 
 const Reel = () => {
   const { showreelRef } = useContext(ScrollContext);
+  const { ref, inView } = useInView({
+    triggerOnce: true, // Ensures the count up happens once
+    threshold: 0.5, // Triggers when 50% of the element is in view
+  });
   return (
-    <section  ref={showreelRef} className="w-full ">
-      <div className="">
-        <iframe
-          src="https://www.youtube.com/embed/NcBjx_eyvxc?si=0-0IOo4Pb1ncuMzZ"
-          title="showreel"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-          className="w-full h-screen"
-        ></iframe>
-      </div>
-    </section>
+    <>
+      {/* <div className="bg-white min-h-[30vh] text-black text-center text-2xl flex p-8">
+      <h1 ref={ref} className="max-w-screen-xl mx-auto my-auto">
+        {inView && (
+          <span>
+            <CountUp start={1} end={99} duration={3.5} /> +
+          </span>
+        )}
+        {""} videos produced and edited,{" "}
+        <span>
+          <CountUp start={1} end={5} duration={3.5} />
+        </span>{" "}
+        years of experience.
+      </h1>
+        </div> */}
+      <section  ref={showreelRef} className="w-full ">
+        <div className="">
+          <iframe
+            src="https://www.youtube.com/embed/NcBjx_eyvxc?si=0-0IOo4Pb1ncuMzZ"
+            title="showreel"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+            className="w-full h-screen"
+          ></iframe>
+        </div>
+      </section>
+    </>
   );
 };
 
